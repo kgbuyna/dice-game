d0fd758434092b780f9f5584a038e5bb2ec74450
Kgb <kgbuyna692@gmail.com>
2021-01-24 15:00:34 +0800

    tglogchiin eeljiig soliv

--- dice1.js
+++ dice1.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 0;
+var activePlayer = 1;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -8,58 +8,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+
+// <div class="player-score" id="score-0">43</div>
+// window.document.querySelector("#score-0").textContent = dice;
+
+// document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
 // Програм эхлэхэд бэлтгэе
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
 
-var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
 
-document.querySelector(".btn-roll").addEventListener("click", function() {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  diceDom.style.display = "block";
-  diceDom.src = "dice-" + diceNumber + ".png";
-   
-  if (diceNumber !== 1) {
-    // dice number 1s ylgaatai buuh uyd
-    roundScore = roundScore + diceNumber
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+document.querySelector(".dice").style.display = "none";
 
-  }
-  // 1 buuh uyd 
-  else{
-    switchSide();
-    roundScore = 0;
-  }
-});
-// hold tovchiig ajiluulah
-// hold tovchiig darhad current dr bgaa onoog deesh nemeh
-document.querySelector(".btn-hold").addEventListener("click", function(){
-  scores[activePlayer] = scores[activePlayer] + roundScore;
-  document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
-  if(scores[activePlayer] > 20){
-    document.getElementById("name-" + activePlayer).textContent = "Winner";
-    
-  }
-  // roundScore 0 boloh
-  roundScore = 0; 
-  diceDom.style.display = "none";
-  switchSide();
-  
-})
-function switchSide(){
-  document.getElementById("current-" + activePlayer).textContent = 0;
-    if(activePlayer ===0){
-      activePlayer = 1;
-    }
-    else{
-      activePlayer = 0;
-    }
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-}
-
+console.log("Шоо :" + dice);
